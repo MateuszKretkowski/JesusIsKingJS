de3ef890213335b299012fd5e289270bdfc14729
MateuszKretkowski <mkretkowski13@gmail.com>
2024-01-22 23:52:43 +0100

    Firebase

--- src/Components/SideBar/sidebar.js
+++ src/Components/SideBar/sidebar.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import "./sidebar.css";
+import {
+    getFirestore,
+} from 'firebase/firestore'
 
 function SideBar() {
   return (
     <div className='sidebar'>
         <div className='sidebar_container'>
-            
+
         </div>
     </div>
   )
